17ee6c76130539c8bcb74568f62850c84d9c15c3
Tomasz Maciejewski <tmaciejewsk@gmail.com>
2016-12-14 17:26:31 +0100

    use named tables

--- db/src/see_db_srv.erl
+++ db/src/see_db_srv.erl
@@ -43,38 +43,38 @@ search(Phrases) when is_binary(Phrases) ->
 %----------------------------------------------------------
 
 init(_Args) ->
-    PagesTid = ets:new(see_pages, [{keypos, #page.id}]),
-    IndexTid = ets:new(see_index, [{keypos, #index.word}]),
-    {ok, {PagesTid, IndexTid}}.
+    ets:new(see_pages, [named_table, {keypos, #page.id}]),
+    ets:new(see_index, [named_table, {keypos, #index.word}]),
+    {ok, []}.
 
 terminate(_, _) ->
     ok.
 
-handle_cast({visited, URL, {error, Reason}}, {PagesTid, IndexTid}) ->
+handle_cast({visited, URL, {error, Reason}}, State) ->
     Id = erlang:phash2(URL),
-    remove_from_index(IndexTid, PagesTid, Id),
-    ets:insert(PagesTid, #page{id = Id, url = URL, words = {error, Reason}}),
-    {noreply, {PagesTid, IndexTid}};
+    remove_from_index(Id),
+    ets:insert(see_pages, #page{id = Id, url = URL, words = {error, Reason}}),
+    {noreply, State};
 
-handle_cast({visited, URL, binary}, {PagesTid, IndexTid}) ->
+handle_cast({visited, URL, binary}, State) ->
     Id = erlang:phash2(URL),
-    remove_from_index(IndexTid, PagesTid, Id),
-    ets:insert(PagesTid, #page{id = Id, url = URL, words = binary}),
-    {noreply, {PagesTid, IndexTid}};
+    remove_from_index(Id),
+    ets:insert(see_pages, #page{id = Id, url = URL, words = binary}),
+    {noreply, State};
 
-handle_cast({visited, URL, {redirect, RedirectURL}}, {PagesTid, IndexTid}) ->
+handle_cast({visited, URL, {redirect, RedirectURL}}, State) ->
     Id = erlang:phash2(URL),
-    remove_from_index(IndexTid, PagesTid, Id),
-    ets:insert(PagesTid, #page{id = Id, url = URL, words = {redirect, RedirectURL}}),
-    {noreply, {PagesTid, IndexTid}};
+    remove_from_index(Id),
+    ets:insert(see_pages, #page{id = Id, url = URL, words = {redirect, RedirectURL}}),
+    {noreply, State};
 
-handle_cast({visited, URL, RawWords}, {PagesTid, IndexTid}) ->
+handle_cast({visited, URL, RawWords}, State) ->
     Id = erlang:phash2(URL),
-    remove_from_index(IndexTid, PagesTid, Id),
+    remove_from_index(Id),
     Words = process_words(RawWords),
-    ets:insert(PagesTid, #page{id = Id, url = URL, words = Words}),
-    insert_to_index(IndexTid, Words, Id),
-    {noreply, {PagesTid, IndexTid}};
+    ets:insert(see_pages, #page{id = Id, url = URL, words = Words}),
+    lists:foreach(fun(Word) -> insert_to_index(Word, Id) end, Words),
+    {noreply, State};
 
 handle_cast(_, State) ->
     {noreply, State}.
@@ -82,28 +82,28 @@ handle_cast(_, State) ->
 handle_call(stop, _, State) ->
     {stop, shutdown, ok, State};
 
-handle_call({queue, URL}, _, {PagesTid, _} = State) ->
+handle_call({queue, URL}, _, State) ->
     case sanitize_url(URL) of
         {ok, SanitizedURL} ->
-            queue_url(SanitizedURL, PagesTid),
+            queue_url(SanitizedURL),
             {reply, ok, State};
         error ->
             {reply, error, State}
     end;
 
-handle_call(next, _, {PagesTid, _} = State) ->
-    case ets:match(PagesTid, #page{last_visit = null, url = '$1', _ = '_'}, 1) of
+handle_call(next, _, State) ->
+    case ets:match(see_pages, #page{last_visit = null, url = '$1', _ = '_'}, 1) of
         {[[URL]], _} ->
             {reply, {ok, URL}, State};
         _ ->
             {reply, nothing, State}
     end;
 
-handle_call({search, Phrases}, _, {PagesTid, IndexTid} = State) ->
+handle_call({search, Phrases}, _, State) ->
     Words = process_words(binary:split(Phrases, <<" ">>, [global, trim_all])),
-    PageLists = [get_pages(Word, IndexTid) || Word <- Words],
+    PageLists = [get_pages(Word) || Word <- Words],
     Result = merge_page_lists(PageLists),
-    {reply, [get_url(Id, PagesTid) || Id <- Result], State};
+    {reply, [get_url(Id) || Id <- Result], State};
 
 handle_call(_, _, State) ->
     {noreply, State}.
@@ -126,49 +126,45 @@ sanitize_url(URL) ->
             {ok, mochiweb_util:urlunsplit({Scheme, Netloc, Path, Query, []})}
     end.
 
-queue_url(URL, PagesTid) ->
+queue_url(URL) ->
     Id = erlang:phash2(URL),
-    case ets:lookup(PagesTid, Id) of
+    case ets:lookup(see_pages, Id) of
         [] ->
-            ets:insert(PagesTid, #page{id = Id, url = URL, last_visit = null});
+            ets:insert(see_pages, #page{id = Id, url = URL, last_visit = null});
         _ ->
             ok
     end.
 
-remove_from_index(IndexTid, PagesTid, Id) ->
-    case ets:lookup(PagesTid, Id) of
+remove_from_index(Id) ->
+    case ets:lookup(see_pages, Id) of
         [#page{words = Words}] when is_list(Words) ->
-            lists:foreach(fun(Word) -> remove_from_word(IndexTid, Word, Id) end, Words);
+            lists:foreach(fun(Word) -> remove_from_word(Word, Id) end, Words);
         _Other ->
             ok
     end.
 
-remove_from_word(IndexTid, Word, Id) ->
-    case ets:lookup(IndexTid, Word) of
+remove_from_word(Word, Id) ->
+    case ets:lookup(see_index, Word) of
         [] ->
             ok;
         [#index{pages = Pages}] ->
-            ets:insert(IndexTid, #index{word = Word, pages = sets:del_element(Id, Pages)})
+            ets:insert(see_index, #index{word = Word, pages = sets:del_element(Id, Pages)})
     end.
 
-insert_to_index(_, [], _) ->
-    ok;
-
-insert_to_index(IndexTid, [Word | Words], Id) ->
-    case ets:lookup(IndexTid, Word) of
+insert_to_index(Word, Id) ->
+    case ets:lookup(see_index, Word) of
         [#index{pages = Pages}] ->
-            ets:insert(IndexTid, #index{word = Word, pages = sets:add_element(Id, Pages)});
+            ets:insert(see_index, #index{word = Word, pages = sets:add_element(Id, Pages)});
         [] ->
-            ets:insert(IndexTid, #index{word = Word, pages = sets:from_list([Id])})
-    end,
-    insert_to_index(IndexTid, Words, Id).
+            ets:insert(see_index, #index{word = Word, pages = sets:from_list([Id])})
+    end.
 
-get_url(Id, PagesTid) ->
-    [#page{url = URL}] = ets:lookup(PagesTid, Id),
+get_url(Id) ->
+    [#page{url = URL}] = ets:lookup(see_pages, Id),
     URL.
 
-get_pages(Word, IndexTid) ->
-    case ets:lookup(IndexTid, Word) of
+get_pages(Word) ->
+    case ets:lookup(see_index, Word) of
         [] ->
             sets:new();
         [#index{pages = Pages}] ->
