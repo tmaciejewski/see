6d6bbe7b739d1098f5e3c80fb48db6ec96b1131d
Tomasz Maciejewski <tmaciejewsk@gmail.com>
2016-12-15 21:17:36 +0100

    add specs to see_text

--- db/src/see_text.erl
+++ db/src/see_text.erl
@@ -2,17 +2,21 @@
 
 -export([extract_words/1]).
 
+-spec extract_words(iodata()) -> list(binary()).
 extract_words(Data) ->
     StrippedData = strip_nonalpha_characters(Data),
     Words = split_to_words(StrippedData),
     lists:filtermap(fun process_word/1, Words).
 
+-spec strip_nonalpha_characters(iodata()) -> iodata().
 strip_nonalpha_characters(Data) ->
     re:replace(Data, <<"[^A-Za-z0-9]">>, <<" ">>, [global]).
 
+-spec split_to_words(iodata()) -> list(binary()).
 split_to_words(Data) ->
-    re:split(Data, <<" ">>).
+    re:split(Data, <<" ">>, [{return, binary}]).
 
+-spec process_word(binary()) -> false | {ok, binary()}.
 process_word(Word) when size(Word) < 2 ->
     false;
 
