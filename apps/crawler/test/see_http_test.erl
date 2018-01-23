2429f237fdc1c3a741960c634b63843d7dd93cb5
Tomasz Maciejewski <tmaciejewsk@gmail.com>
2018-01-23 18:23:55 +0100

    change expect_http_request() to mock_http_response()

--- apps/crawler/test/see_http_test.erl
+++ apps/crawler/test/see_http_test.erl
@@ -11,14 +11,14 @@ stop(_) ->
     ?assert(meck:validate(hackney)),
     meck:unload(hackney).
 
-expect_http_request(URL, Result) ->
+mock_http_response(URL, Result) ->
     meck:expect(hackney, request, [{[get, URL, '_', '_', '_'], Result}]).
 
 http_error_test_() ->
     {setup, fun start/0, fun stop/1,
      fun(_) ->
              Reason = "reason",
-             expect_http_request(?URL, {error, Reason}),
+             mock_http_response(?URL, {error, Reason}),
              ?_assertEqual({error, Reason}, see_http:get_page(?URL))
      end}.
 
@@ -26,7 +26,7 @@ no_content_type_test_() ->
     {setup, fun start/0, fun stop/1,
      fun(_) ->
              Headers = [],
-             expect_http_request(?URL, {ok, 200, Headers, make_ref()}),
+             mock_http_response(?URL, {ok, 200, Headers, make_ref()}),
              ?_assertEqual(binary, see_http:get_page(?URL))
      end}.
 
@@ -34,7 +34,7 @@ binary_blob_test_() ->
     {setup, fun start/0, fun stop/1,
      fun(_) ->
              Headers = [{<<"Content-Type">>, <<"application/octet-stream">>}],
-             expect_http_request(?URL, {ok, 200, Headers, make_ref()}),
+             mock_http_response(?URL, {ok, 200, Headers, make_ref()}),
              ?_assertEqual(binary, see_http:get_page(?URL))
      end}.
 
@@ -43,19 +43,19 @@ redirect_test_() ->
      [fun(_) ->
               RedirectURL = <<"redirect url">>,
               Headers = [{<<"Location">>, RedirectURL}],
-              expect_http_request(?URL, {ok, 301, Headers, make_ref()}),
+              mock_http_response(?URL, {ok, 301, Headers, make_ref()}),
               ?_assertEqual({redirect, RedirectURL}, see_http:get_page(?URL))
       end,
       fun(_) ->
               RedirectURL = <<"redirect url">>,
               Headers = [{<<"Location">>, RedirectURL}],
-              expect_http_request(?URL, {ok, 302, Headers, make_ref()}),
+              mock_http_response(?URL, {ok, 302, Headers, make_ref()}),
               ?_assertEqual({redirect, RedirectURL}, see_http:get_page(?URL))
       end,
       fun(_) ->
               RedirectURL = <<"redirect url">>,
               Headers = [{<<"other-header">>, <<"foo">>}],
-              expect_http_request(?URL, {ok, 302, Headers, make_ref()}),
+              mock_http_response(?URL, {ok, 302, Headers, make_ref()}),
               ?_assertEqual({error, Headers}, see_http:get_page(?URL))
       end]}.
 
@@ -64,7 +64,7 @@ unknown_code_test_() ->
      fun(_) ->
              Headers = headers,
              Code = 666,
-             expect_http_request(?URL, {ok, Code, Headers, make_ref()}),
+             mock_http_response(?URL, {ok, Code, Headers, make_ref()}),
              ?_assertEqual({error, {Code, Headers}}, see_http:get_page(?URL))
      end}.
 
@@ -74,7 +74,7 @@ ok_code_test_() ->
              Content = <<"content">>,
              Headers = [{<<"Content-Type">>, <<"text/plain">>}],
              Ref = make_ref(),
-             expect_http_request(?URL, {ok, 200, Headers, Ref}),
+             mock_http_response(?URL, {ok, 200, Headers, Ref}),
              meck:expect(hackney, body, [{[Ref, '_'], {ok, Content}}]),
              ?_assertEqual({ok, Content}, see_http:get_page(?URL))
      end}.
